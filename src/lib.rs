675a1d9f9207f8eb701f8d19ef1825bcb392a024
efran <efranhimel25@gmail.com>
2026-04-26 10:57:00 -0400

    threadpool use workers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
-use std::fmt::{Formatter, Display, Debug};
+use std::fmt::{Debug, Display, Formatter};
+use std::{sync::mpsc, thread};
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
-pub struct ThreadPool;
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
 
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
+    }
+}
 pub struct PoolCreationError;
 
-impl Display for PoolCreationError{
+impl Display for PoolCreationError {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         write!(f, "ThreadPool size must be greater than zero")
     }
@@ -25,19 +38,26 @@ impl ThreadPool {
     ///
     /// The `new` function will panic if the size is zero.
 
-    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
-        if size > 0 {
-            Ok(ThreadPool)
-        } else {
-            Err(PoolCreationError)
-        }
-    }
+    // pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+    //     if size > 0 {
+    //         Ok(ThreadPool)
+    //     } else {
+    //         Err(PoolCreationError)
+    //     }
+    // }
 
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+        ThreadPool { workers }
     }
 
-    pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static,
-    {}
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
 }
